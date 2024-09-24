741dfbaae162aa331772a64a14ff8b94aab78870
Franco Osman <francoosman12@gmail.com>
2024-09-24 12:14:14 -0300

    Cambios en trabajos realizados

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -17,19 +17,43 @@ const Slider = () => (
         <img src="https://i.ibb.co/z2z7KJr/Whats-App-Image-2024-09-12-at-18-07-28-1.jpg" alt="Trabajo 4" />
       </div>
       <div className="image-item">
-        <img src="https://i.ibb.co/sJHNNWX/Whats-App-Image-2024-09-12-at-17-57-45-2.jpg" alt="Trabajo 5" />
+        <img src="https://i.ibb.co/gmPSB3W/Whats-App-Image-2024-09-12-at-17-57-45.jpg" alt="Trabajo 5" />
       </div>
       <div className="image-item">
-        <img src="https://i.ibb.co/k9LcTsB/Whats-App-Image-2024-09-12-at-17-57-45-1.jpg" alt="Trabajo 6" />
+        <img src="https://i.ibb.co/sJHNNWX/Whats-App-Image-2024-09-12-at-17-57-45-2.jpg" alt="Trabajo 6" />
       </div>
       <div className="image-item">
-        <img src="https://i.ibb.co/QFBvL6F/Whats-App-Image-2024-09-12-at-17-57-43.jpg" alt="Trabajo 7" />
+        <img src="https://i.ibb.co/k9LcTsB/Whats-App-Image-2024-09-12-at-17-57-45-1.jpg" alt="Trabajo 7" />
       </div>
       <div className="image-item">
-        <img src="https://i.ibb.co/YhqHFGr/Whats-App-Image-2024-09-12-at-17-57-43-1.jpg" alt="Trabajo 8" />
+        <img src="https://i.ibb.co/QFBvL6F/Whats-App-Image-2024-09-12-at-17-57-43.jpg" alt="Trabajo 8" />
       </div>
       <div className="image-item">
-        <img src="https://i.ibb.co/7k7R2GS/Whats-App-Image-2024-09-12-at-17-57-40.jpg" alt="Trabajo 8" />
+        <img src="https://i.ibb.co/YhqHFGr/Whats-App-Image-2024-09-12-at-17-57-43-1.jpg" alt="Trabajo 9" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/hm5k6wv/Whats-App-Image-2024-09-12-at-17-57-42.jpg" alt="Trabajo 10" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/JHgH0jd/Whats-App-Image-2024-09-12-at-17-57-42-2.jpg" alt="Trabajo 11" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/9c4hQsN/Whats-App-Image-2024-09-12-at-17-57-42-1.jpg" alt="Trabajo 12" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/wLBCWq5/Whats-App-Image-2024-09-12-at-17-57-41.jpg" alt="Trabajo 13" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/vwWdzBF/Whats-App-Image-2024-09-12-at-17-57-41-2.jpg" alt="Trabajo 14" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/vsQ5wBD/Whats-App-Image-2024-09-12-at-17-57-41-1.jpg" alt="Trabajo 15" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/7k7R2GS/Whats-App-Image-2024-09-12-at-17-57-40.jpg" alt="Trabajo 16" />
+      </div>
+      <div className="image-item">
+        <img src="https://i.ibb.co/M240Mrs/Whats-App-Image-2024-09-12-at-17-57-40-1.jpg" alt="Trabajo 17" />
       </div>
     </div>
   </section>
